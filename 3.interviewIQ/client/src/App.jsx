34b4754054c6f8e6b9db68cbe9b3734d57766dcc
Satyam Shukla <shuklasatyam843@gmail.com>
2026-02-25 05:25:55 +0530

    Fix case-sensitive Auth file name

--- 3.interviewIQ/client/src/App.jsx
+++ 3.interviewIQ/client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Auth from './pages/auth'
+import Home from './pages/Home.jsx'
+import Auth from './pages/Auth.jsx'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setUserData } from './redux/userSlice'
 import InterviewPage from './pages/InterviewPage'
 import InterviewHistory from './pages/InterviewHistory'
-import Pricing from './pages/Pricing'
+import Pricing from './pages/Pricing.jsx'
 import InterviewReport from './pages/InterviewReport'
 
 export const ServerUrl  = "http://localhost:8000"
