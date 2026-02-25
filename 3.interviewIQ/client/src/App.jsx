396e1c60584e1278f5a3768567337b796724c5d4
Satyam Shukla <shuklasatyam843@gmail.com>
2026-02-25 08:03:17 +0530

    Fix environment variable for Vite frontend

--- 3.interviewIQ/client/src/App.jsx
+++ 3.interviewIQ/client/src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Auth from './pages/Auth.jsx'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setUserData } from './redux/userSlice'
 import InterviewPage from './pages/InterviewPage'
 import InterviewHistory from './pages/InterviewHistory'
-import Pricing from './pages/Pricing.jsx'
+import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
 
+
+export const ServerUrl = import.meta.env.VITE_API_URL;
 function App() {
 
   const dispatch = useDispatch()
