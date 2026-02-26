6fc53756bee82bea848f04fb7bdee23bf2e3cd50
Satyam Shukla <shuklasatyam843@gmail.com>
2026-02-26 06:42:40 +0530

    fix: secure true for cross-domain cookies

--- 3.interviewIQ/server/controllers/auth.controller.js
+++ 3.interviewIQ/server/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const googleAuth = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token" , token , {
            httpOnly: true,
-            secure:false,
+              secure: true, 
            sameSite: "none",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
@@ -30,12 +30,15 @@ export const googleAuth = async (req,res) => {
     
 }
 
-export const logOut = async (req,res) => {
+export const logOut = async (req, res) => {
     try {
-        await res.clearCookie("token")
-        return res.status(200).json({message:"LogOut Successfully"})
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
+        return res.status(200).json({ message: "LogOut Successfully" })
     } catch (error) {
-         return res.status(500).json({message:`Logout error ${error}`})
+        return res.status(500).json({ message: `Logout error ${error}` })
     }
-    
 }
